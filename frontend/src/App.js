d1601900f3d465126be3df1af28e664584e96254
Jennifer Garcia <jen.garcia91@gmail.com>
2020-02-15 13:49:19 -0800

    Made map show up on loading screen Added sdk, styling and added stuff. Yay!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  componentDidMount() { 
+    const script = document.createElement('script');
+    script.src = process.env.PUBLIC_URL + '/sdk/tomtom.min.js';
+    document.body.appendChild(script);
+    script.async = true;
+    script.onload = function () {
+      window.tomtom.L.map('map', {
+        source: 'vector',
+        key: 'uPxVlGdisX4QTfwcvbq47GAEuG0hna5C',
+        center: [37.769167, -122.478468],
+        basePath: '/sdk',
+        zoom: 15
+      });
+    }
+  }
+
+render (){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id = 'map'></div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +38,8 @@ function App() {
       </header>
     </div>
   );
+}
+ 
 }
 
 export default App;
