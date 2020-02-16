59230d33f4d29f47c1de81eb77c15ae9b88431e0
Teddy Amkie <teddyamkie@gmail.com>
2020-02-15 16:01:50 -0800

    Added post route, fixed model styling, added routes to app

--- backend/models/listing.js
+++ backend/models/listing.js
@@ -5,8 +5,15 @@ class Listing {
   // Create a listing from input: 
   // { name, image, description, quantity, user_id, category, location }
   static async create(item) {
+    [image, description, quantity, user_id, category, location] = item;
     const result = await db.query(
-      `INSERT INTO listings (name, image, description, quantity, user_id, category, location)
+      `INSERT INTO listings (name,
+                            image,
+                            description,
+                            quantity,
+                            user_id,
+                            category,
+                            location)
       VALUES ($1, $2, $3, $4, $5, $6, $7 )`,
       [name, image, description, quantity, user_id, category, location]
     );
@@ -22,3 +29,4 @@ class Listing {
   }
 }
 module.exports = Listing;
+
