9e1e4d48faa15e2cbfc919907e07583bee0e9ce2
spokenbird <39173486+spokenbird@users.noreply.github.com>
2020-02-16 12:22:59 -0800

    Add User authentication method

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,15 @@ const db = require('../db');
 const bcrypt = require('bcrypt');
 const ExpressError = require('../helpers/expressError');
 
-let BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
+
+/** Related functions for users. */
 
 class User {
+  /** Register user with data. Returns new user data or throws duplicate error. */
+
   static async register(data) {
-    // Check for duplicate email
+    // check for duplicate email
     const duplicateCheck = await db.query(
       ` SELECT email
         FROM users
@@ -47,6 +51,34 @@ class User {
     );
 
     return result.rows[0];
+  }
+
+  /** Authenticate user with username, password. Returns user or throws error. */
+
+  static async authenticate(data) {
+    // try to find the user first.
+    const result = await db.query(
+      ` SELECT username,
+               password,
+               account_type,
+               email,
+               photo_url
+          FROM users
+          WHERE username = $1`,
+        [data.username]
+    );
+
+    const user = result.rows[0];
+
+    if (user) {
+      // compare hashed password to a new hash from password
+      const isValid = await bcrypt.compare(data.password, user.password);
+      if (isValid) {
+        return user;
+      }
+    }
+    
+    throw ExpressError("Invalid Password", 401);
   }
 }
 
